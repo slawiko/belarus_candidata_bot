c6bb9b0edb5e151017028539e3933b7eacbacdff
slawiko <shchaurouski.slava@gmail.com>
2020-06-17 23:56:30 +0200

    Adds new category "Bio"

--- category.py
+++ category.py
@@ -1,15 +1,19 @@
 CONTACTS_ID = 'contacts'
 INTERVIEWS_ID = 'interviews'
+BIO_ID = 'bio'
 CONTACTS_NAME = 'Кантакты'
 INTERVIEWS_NAME = 'Інтэрв’ю'
+BIO_NAME = 'Біяграфія'
 
 id_name_map = {
     CONTACTS_ID: CONTACTS_NAME,
-    INTERVIEWS_ID: INTERVIEWS_NAME
+    INTERVIEWS_ID: INTERVIEWS_NAME,
+    BIO_ID: BIO_NAME,
 }
 name_id_map = {
     CONTACTS_NAME: CONTACTS_ID,
-    INTERVIEWS_NAME: INTERVIEWS_ID
+    INTERVIEWS_NAME: INTERVIEWS_ID,
+    BIO_NAME: BIO_ID,
 }
 
 
