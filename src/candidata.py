fdc60ca2f4a93c5545cb7e47157fa323e174bf72
slawiko <shchaurouski.slava@gmail.com>
2020-06-21 14:20:00 +0200

    Removes unnecessary map; now candidates are stored under belorussian language keys

--- src/candidata.py
+++ src/candidata.py
@@ -9,35 +9,31 @@ from src.common import create_markup
 
 class Candidata:
     def __init__(self, data_folder="./candidates"):
-        self.cand_name_id_map = {}
-        self.cand_id_name_map = {}
         self.cat_ids = set()
         self.categories = []
-        self.cat_markup = None
+        self.cat_markup = {}
         self.index = {}
         self.markup = None
         self.names = []
 
         self.init(data_folder)
+        self.get_candidates_keyboard()
 
     def init(self, data_folder):
         for filename in listdir(data_folder):
-            candidate_id = filename.split('.')[0]
             with open(join(data_folder, filename), mode='r', encoding='utf-8') as candidate_file:
-                self.read_file(candidate_id, candidate_file)
-        self.names = list(self.cand_name_id_map.keys())
+                self.read_file(candidate_file)
+        self.names = list(self.index.keys())
         self.categories = list(map(category.humanize, self.cat_ids))
 
-    def read_file(self, cand_id, file):
+    def read_file(self, file):
         candidate = toml.loads(file.read())
-        self.cand_name_id_map[candidate['name']] = cand_id
-        self.cand_id_name_map[cand_id] = candidate['name']
+        cand_name = candidate['name']
         del candidate['name']
-        self.index[cand_id] = candidate
+        self.index[cand_name] = candidate
         self.cat_ids.update(list(candidate.keys()))
 
-    def get(self, cand_name, cat_name):
-        cand = self.dehumanize(cand_name)
+    def get(self, cand, cat_name):
         cat = category.dehumanize(cat_name)
         return self.index[cand][cat]
 
@@ -47,23 +43,15 @@ class Candidata:
 
         return self.markup
 
-    def get_categories_keyboard(self):
-        if not self.cat_markup:
-            self.cat_markup = create_markup(self.categories)
+    def get_categories_keyboard(self, cand):
+        if cand not in self.cat_markup:
+            self.cat_markup[cand] = create_markup(list(map(category.humanize, self.index[cand].keys())))
 
-        return self.cat_markup
+        return self.cat_markup[cand]
 
     def get_categories_names(self):
         categories = list(map(lambda x: category.id_name_map[x], self.categories))
         return create_markup(categories)
-
-    def humanize(self, cand_id):
-        cand_name = self.cand_name_id_map[cand_id]
-        return cand_name
-
-    def dehumanize(self, cand_name):
-        cand_id = self.cand_name_id_map[cand_name]
-        return cand_id
 
 
 data = Candidata()
