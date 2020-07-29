27036c4b12f1d6a93e0bf6706cb76ffdcfdf37d7
slawiko <shchaurouski.slava@gmail.com>
2020-07-29 12:11:55 +0300

    Fixes type; adds program

--- src/category.py
+++ src/category.py
@@ -2,19 +2,22 @@ CONTACTS_ID = 'contacts'
 TEXT_INTERVIEWS_ID = 'text-interviews'
 VIDEO_INTERVIEWS_ID = 'video-interviews'
 BIO_ID = 'bio'
+PROGRAM_ID = 'program'
 SOME_ID = 'some'
 
 CONTACTS_NAME = 'Кантакты'
 TEXT_INTERVIEWS_NAME = 'Тэкставыя інтэрв\'ю'
 VIDEO_INTERVIEWS_NAME = 'Відэа-інтэрв\'ю'
 BIO_NAME = 'Біяграфія'
-SOME_NAME = 'Што небудзь'
+PROGRAM_NAME = 'Праграма'
+SOME_NAME = 'Што-небудзь'
 
 id_name_map = {
     CONTACTS_ID: CONTACTS_NAME,
     TEXT_INTERVIEWS_ID: TEXT_INTERVIEWS_NAME,
     VIDEO_INTERVIEWS_ID: VIDEO_INTERVIEWS_NAME,
     BIO_ID: BIO_NAME,
+    PROGRAM_ID: PROGRAM_NAME,
     SOME_ID: SOME_NAME,
 }
 name_id_map = {
@@ -22,6 +25,7 @@ name_id_map = {
     TEXT_INTERVIEWS_NAME: TEXT_INTERVIEWS_ID,
     VIDEO_INTERVIEWS_NAME: VIDEO_INTERVIEWS_ID,
     BIO_NAME: BIO_ID,
+    PROGRAM_NAME: PROGRAM_ID,
     SOME_NAME: SOME_ID,
 }
 
